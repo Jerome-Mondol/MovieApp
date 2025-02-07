1647a12ef9d6c4c43fc6ac6ca7af7db85c0e983c
Jerome Mondol <jeromemondol11@gmail.com>
2025-02-07 22:35:37 +0600

    header part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { Props } from './Card'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div>
-        <Props name="Star wars" age="3 star"/>
-        <Props name="Avatar" age="4 star"/>
-        <Props name="The Lion King" age="3.9 star"/>
-    </div>
+    <main>
+        <div className='pattern' />
+
+        <div className='wrapper' >
+            <header>
+                {/* Header Image */}
+                 <img src="./hero.png" alt="Hero Banner" />
+
+                {/* Hero text */}
+                <h1 className='' >Find <span className='text-gradient' >Movies</span> You'll Enjoy Without the Hassel</h1>
+
+                {/* Search */}
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </header>
+        </div>
+    </main>
   )
 }
 
