8f44a8314c0ec00742ba194efecedbe9b14435b5
Jerome Mondol <jeromemondol11@gmail.com>
2025-02-07 21:13:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Props } from './Card'
+
+const App = () => {
+  return (
+    <div>
+        <Props name="Star wars" age="3 star"/>
+        <Props name="Avatar" age="4 star"/>
+        <Props name="The Lion King" age="3.9 star"/>
+    </div>
+  )
+}
+
+export default App
